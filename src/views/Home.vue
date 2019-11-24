4187e4af8bf9b38127a5147194a1ec70dddd711c
Omar Morales <saul090594@gmail.com>
2019-11-24 02:54:52 -0600

    education section added | contact with drift service

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,21 +15,21 @@
 
                     <v-flex mb-4>
                         <h1 class="display-2 font-weight-bold mb-1">
-                        Omar Saúl Morales Ibarra
+                            Omar Saúl Morales Ibarra
                         </h1>
                         <vue-typer
-                        class="headline"
-                        :text='positions'
-                        :repeat='Infinity'
-                        :shuffle='false'
-                        initial-action='typing'
-                        :pre-type-delay='70'
-                        :type-delay='70'
-                        :pre-erase-delay='2000'
-                        :erase-delay='70'
-                        erase-style='backspace'
-                        :erase-on-complete='false'
-                        caret-animation='blink'
+                            class="headline"
+                            :text='positions'
+                            :repeat='Infinity'
+                            :shuffle='false'
+                            initial-action='typing'
+                            :pre-type-delay='70'
+                            :type-delay='70'
+                            :pre-erase-delay='2000'
+                            :erase-delay='70'
+                            erase-style='backspace'
+                            :erase-on-complete='false'
+                            caret-animation='blink'
                         ></vue-typer>
                     </v-flex>
                 </v-layout>
@@ -51,12 +51,36 @@
         </v-row>
         <v-row>
             <v-col cols="12" md="12">
-                <h2 class="title">Proyectos</h2>
+                <h2 class="display-1 text-uppercase font-weight-bold mb-3"><i class="fas fa-graduation-cap"></i> Educacion</h2>
+                <v-timeline dense>
+                    <v-timeline-item
+                        class="mb-4"
+                        color="white"
+                        fill-dot
+                    >
+                        <template v-slot:icon>
+                            <v-avatar>
+                                <img 
+                                    :src="require('../assets/itesm-logo.jpeg')"
+                                >
+                            </v-avatar>
+                        </template>
+                        <v-row class="pt-1">
+                            <v-col cols="12" md="3" lg="2">
+                                <strong>2012 - 2016</strong>
+                            </v-col>
+                            <v-col>
+                                <p class="font-weight-bold mb-0">Instituto Tecnológico y de Estudios Superiores de Monterrey (ITESM)</p>
+                                <p class="mb-0">Ingenieria en Negocios y Tecnologias de Informacion</p>
+                            </v-col>
+                        </v-row>
+                    </v-timeline-item>
+                </v-timeline>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="12" md="12">
-                <h2 class="title">Publicaciones</h2>
+                <h2 class="display-1 text-uppercase font-weight-bold mb-3"><i class="fas fa-award"></i> Cursos y certificaciones</h2>
             </v-col>
         </v-row>
     </v-container>
